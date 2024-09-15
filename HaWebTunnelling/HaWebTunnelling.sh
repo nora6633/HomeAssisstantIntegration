d548725d8965f4866605dc8ef600869e368e8eeb
Krixi <101371329+krixi0131@users.noreply.github.com>
2024-09-15 11:54:32 +0800

    Update HaWebTunnelling.sh, add connection timeout in curl

--- HaWebTunnelling/HaWebTunnelling.sh
+++ HaWebTunnelling/HaWebTunnelling.sh
@@ -3,7 +3,7 @@
 # this script is used for port forwarding the HA web from this private ip only host to a host have public ip
 
 # Check if any process contains "homeassistant.local"
-if curl -s --head --request GET http://163.22.17.184:8123 | grep "200 OK" > /dev/null; then
+if curl -s --head --request GET http://163.22.17.184:8123 --max-time 5 | grep "200 OK" > /dev/null; then
     echo "`date` : ssh tunnelling process is running." >> ./result.txt
 else
     echo "`date` : No ssh tunnelling process found" >> ./result.txt
